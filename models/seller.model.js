b0ae08e5c0df3ce5f2567c5c35b6afea8d9c18e7
kkcchow <joejoe020120@gmail.com>
2022-10-08 23:52:37 +0800

    modify seller schema

--- models/seller.model.js
+++ models/seller.model.js
@@ -1,9 +1,28 @@
 const mongoose = require("mongoose");
 
 const SellerSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+
   name: {
     type: String,
     required: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  contactnumber: {
+    type: String,
+    required: true,
+  },
+
+  currentlocation: {
+    type: String,
   },
 });
 
